cee2986549dc05e3412eb3f668c8b5fc594e0abf
jmishra1927 <jmishra1927@gmail.com>
2023-11-13 18:07:09 +0530

    "Handle null case and rename method"

--- src/MinimumOfArray.java
+++ src/MinimumOfArray.java
@@ -2,10 +2,13 @@ public class MinimumOfArray {
 
     public static void main(String[] args) {
         int[] arr = {4, 5, 15, 25, 36, 48, 5, 6, 1, -20};
-        MinimumOfArray.minOfArray(arr);
+        MinimumOfArray.findMinimume(arr);
     }
 
-    private static void minOfArray(int[] arr) {
+    private static void findMinimume(int[] arr) {
+        if(arr == null || arr.length == 0){
+            throw new IllegalArgumentException("Invalid input. Input array is null or blank");
+        }
         int min = arr[0];
         for(int i = 1; i< arr.length; i++){
             if(min > arr[i]){
