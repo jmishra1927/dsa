b75c870f8f93c16ab1b438f93a83cadef917c659
jmishra1927 <jmishra1927@gmail.com>
2023-11-13 18:02:31 +0530

    "Fined minimum of given array"

--- src/MinimumOfArray.java
+++ src/MinimumOfArray.java
@@ -6,7 +6,7 @@ public class MinimumOfArray {
     }
 
     private static void minOfArray(int[] arr) {
-        int min =  arr[0];
+        int min = arr[0];
         for(int i = 1; i< arr.length; i++){
             if(min > arr[i]){
                 min = arr[i];
