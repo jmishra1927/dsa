fc34b24987adf852af081ad990f38c4992dacec1
jmishra1927 <jmishra1927@gmail.com>
2023-11-22 18:21:28 +0530

    Added code to pring elements of LinkedList

--- singly-lined-list/src/SinglyLinkedList.java
+++ singly-lined-list/src/SinglyLinkedList.java
@@ -10,4 +10,25 @@ public class SinglyLinkedList {
             this.next = null;
         }
     }
+
+    public static void main(String[] args) {
+        SinglyLinkedList sll = new SinglyLinkedList();
+        sll.head = new ListNode(10);
+        ListNode second = new ListNode(1);
+        ListNode third = new ListNode(8);
+        ListNode fourth = new ListNode(11);
+
+        //Assign reference of one node to another
+        sll.head.next = second;  //10 -->1
+        second.next = third;   //10 --> 1 --> 8
+        third.next = fourth;     //10-->1-->8-->11
+
+        //Print elements of Singly Linked List
+        ListNode current = sll.head;
+        while(current != null){
+            System.out.print(current.data+ " --> ");
+            current = current.next;
+        }
+        System.out.println("null");
+    }
 }
